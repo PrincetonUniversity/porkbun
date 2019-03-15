64f7e1d01d4b3990eb05c3d2cabbed5dfb67e5eb
khatna <khatna@github.com>
2019-03-15 18:20:53 -0400

    select meals using cheerio, write to temp

--- src/scripts/scraper.js
+++ src/scripts/scraper.js
@@ -1,3 +1,4 @@
+const cheerio = require('cheerio');
 const axios = require('axios');
 const fs = require('fs');
 const url = 'http://menus.princeton.edu/dining/_Foodpro/online-menu/menuDetails.asp?sName=Princeton+University+Campus+Dining&locationNum=02&locationName=Butler+%26+Wilson+Colleges&naFlag=1';
@@ -5,12 +6,26 @@ const url = 'http://menus.princeton.edu/dining/_Foodpro/online-menu/menuDetails.
 var scrape = function scrape() {
   axios.get(url)
     .then(response => {
-      fs.writeFile('./temp', response.data, function(err) {
-        if (err) {
-          return console.log(err);
-        }
-        console.log("Success");
+      let meals = [];
+      let $ = cheerio.load(response.data);
+      $('.recipe').each((i, recipe) => {
+        meals.push($(recipe).text()+'\n');
       });
+
+      // have to clear the file, since i used appendFile (there's probably a better way)
+      fs.writeFile('./temp', '', err => {
+        if (err)
+          return console.log(err)  
+        }
+      );
+
+      meals.forEach(meal => {
+        fs.appendFile('./temp', meal, err => {
+          if (err) 
+            return console.log(err);
+        });
+      });
+      console.log("Success");
     });
 }
 
