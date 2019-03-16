ebf9c9b94aaf2a105accffc0c40da7e27120471c
Justin Chang <justinjunochang@gmail.com>
2019-03-15 22:32:16 -0400

    allow custom port input

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+// Load node modules
 const express = require('express');
 const scraper = require('./scripts/scraper.js');
+
 const app = express();
 
+// GET request handling
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function() {
+// Start server
+const port = process.argv[2] ? process.argv[2] : 3000;
+app.listen(port, function() {
   scraper.scrape();
-  console.log("Example app listening on port 3000!");
+  console.log("Listening on %d", port);
 });
