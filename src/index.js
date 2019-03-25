921da80b4faa4102d0dbd29cbadc720488ab8686
Justin Chang <justinjunochang@gmail.com>
2019-03-25 15:19:10 -0400

    replace cron with node-cron

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 // Load node modules
 const express = require('express');
 const ejs = require('ejs');
-const cron = require('cron');
+const cron = require('node-cron');
 const scraper = require('./scripts/scraper.js');
-const mongodb = require('./scripts/db.js');
+const db = require('./scripts/db.js');
 
 const app = express();
 
 // Express configs (have to use views/ directory to use res.render)
 app.engine('html', ejs.renderFile);
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/dist'));
+app.set('views', __dirname + '/dist/views');
+app.use(express.static(__dirname + '/static'));
 
 // GET request handling
 app.get('/', function(req, res) {
@@ -26,11 +26,14 @@ app.get(/\/.+/, function(req, res) {
 // Start server
 const port = process.argv[2] ? process.argv[2] : 3000;
 app.listen(port, function() {
-  // const job = new cron.CronJob('0 0 0 * * *', function() {
-  //   console.log('Code for running every midnight');
-  // }, null, false, 'America/New_York');
-  // job.start();
-  scraper.scrape();
-  mongodb.testInsert();
+  cron.schedule('0 0 0 * * *', () => {
+    console.log('Code for running every midnight');
+    
+  }, {
+    timezone: 'America/New_York'
+  });
+  
+  scraper.scrapeIntoDB();
+  //db.updateDish("turkey", "03", "lunch");
   console.log("Listening on %d", port);
 });
