ed67e9a3961702e69d74a17108b78bb109a14b33
khatna <bold@princeton.edu>
2019-03-29 18:48:48 -0400

    use env variable for host name

--- src/config.js
+++ src/config.js
@@ -2,8 +2,6 @@
 
 // domain and port
 const port = process.env.PORT || 3000;
-const host = `http://localhost:${port}`;
+const host = process.env.HOST || `http://localhost:${port}`;
 module.exports.port = port;
 module.exports.host = host;
-
-// we should add code for connecting to the database etc here
